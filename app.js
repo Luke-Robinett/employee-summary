031e66d7dfbd772c7cbad906efa9dd2eb09efde3
Luke Robinett <lukebootcamp@gmail.com>
2020-01-10 21:46:26 -0800

    Implemented full inquirer routine for manager and employees, utilizing the inquirer-recursive plugin

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+inquirer.registerPrompt("recursive", require("inquirer-recursive"));
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,40 +11,73 @@ function init() {
 
 // Gather user input
 function getEmployees() {
+ // First get manager info
  inquirer.prompt(
   [
    {
-    message: "Enter manager's name",
-    name: "mgrName"
+    message: "Manager's name?",
+    name: "name"
    },
    {
-    message: "Enter manager's ID",
-    name: "mgrId"
+    message: "Manager's ID?",
+    name: "id"
    },
    {
-    message: "Enter manager's email address",
-    name: "mgrEmail"
+    message: "Manager's email address?",
+    name: "email"
    },
    {
-    message: "Enter manager's office number",
-    name: "mgrOfficeNo"
-   },
-   {
-    message: "How many employees?",
-    name: "empCount"
+    message: "Manager's office number?",
+    name: "officeNumber"
    }
   ]
  )
- .then(answers => {
-  const mgr = new Manager(
-   answers.mgrName,
-   answers.mgrId,
-   answers.mgrEmail,
-   answers.mgrOfficeNo
-  );
-
-  console.log(mgr);
- });
+  .then(mgrAnswers => {
+   console.log(mgrAnswers);
+   // Now get the info for the team members
+   inquirer.prompt(
+    [
+     {
+      type: "recursive",
+      name: "employees",
+      message: "Enter a new employee?",
+      prompts:
+       [
+        {
+         message: "Employee's role?",
+         name: "role",
+         type: "rawlist",
+         choices: [ "Intern", "Engineer" ]
+        },
+        {
+         message: "Employee's name?",
+         name: "name"
+        },
+        {
+         message: "Employee's ID?",
+         name: "id"
+        },
+        {
+         message: "Employee's email?",
+         name: "email"
+        },
+        {
+         when: answ => (answ.role === "Engineer"),
+         message: "GitHub user name?",
+         name: "github"
+        },
+        {
+         when: answ => (answ.role === "Intern"),
+         message: "Intern's school?",
+         name: "school"
+        }
+       ]
+     }
+    ]
+   ).then(empAnswers => {
+    console.log(empAnswers.employees);
+   });
+  });
 }
 
 // Start the app
