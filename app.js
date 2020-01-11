69dc92c4691ad0e9aeff950fe76e1b8f53ace93d
Luke Robinett <lukebootcamp@gmail.com>
2020-01-10 22:20:08 -0800

    Placed inquirer questions in seperate js module to clean up and streamline the main app.js code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 inquirer.registerPrompt("recursive", require("inquirer-recursive"));
+const questions = require("./lib/questions");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -11,73 +12,14 @@ function init() {
 
 // Gather user input
 function getEmployees() {
- // First get manager info
- inquirer.prompt(
-  [
-   {
-    message: "Manager's name?",
-    name: "name"
-   },
-   {
-    message: "Manager's ID?",
-    name: "id"
-   },
-   {
-    message: "Manager's email address?",
-    name: "email"
-   },
-   {
-    message: "Manager's office number?",
-    name: "officeNumber"
-   }
-  ]
- )
-  .then(mgrAnswers => {
-   console.log(mgrAnswers);
-   // Now get the info for the team members
-   inquirer.prompt(
-    [
-     {
-      type: "recursive",
-      name: "employees",
-      message: "Enter a new employee?",
-      prompts:
-       [
-        {
-         message: "Employee's role?",
-         name: "role",
-         type: "rawlist",
-         choices: [ "Intern", "Engineer" ]
-        },
-        {
-         message: "Employee's name?",
-         name: "name"
-        },
-        {
-         message: "Employee's ID?",
-         name: "id"
-        },
-        {
-         message: "Employee's email?",
-         name: "email"
-        },
-        {
-         when: answ => (answ.role === "Engineer"),
-         message: "GitHub user name?",
-         name: "github"
-        },
-        {
-         when: answ => (answ.role === "Intern"),
-         message: "Intern's school?",
-         name: "school"
-        }
-       ]
-     }
-    ]
-   ).then(empAnswers => {
-    console.log(empAnswers.employees);
-   });
+ inquirer.prompt(questions.mgrQuestions)
+ .then(mgrAnswers => {
+  console.log(mgrAnswers);
+  inquirer.prompt(questions.empQuestions)
+  .then(empAnswers => {
+   console.log(empAnswers);
   });
+ });
 }
 
 // Start the app
