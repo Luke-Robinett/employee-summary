8ae8d8b822d7078be8c51f5ef105a7e4e4addc08
Luke Robinett <lukebootcamp@gmail.com>
2020-01-11 18:36:30 -0800

    Streamlined app.js program flow with an init() entry point and removed unneeded code

--- app.js
+++ app.js
@@ -6,55 +6,24 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Team = require("./lib/Team");
 
-// Gather user input
-function getEmployees() {
+// App entry point
+function init() {
+ // First prompt user for manager's info
  inquirer.prompt(questions.mgrQuestions)
  .then(mgrAnswers => {
+  // Create a new Team object and populate it with the manager's info
   const devTeam = new Team(new Manager());
   devTeam.importManager(mgrAnswers);
+
+  // Next, prompt user to enter info about employees
   inquirer.prompt(questions.empQuestions)
   .then(empAnswers => {
+   // Use these answers to add employees to the Team object
    devTeam.importEmployees(empAnswers.employees);
    console.log(devTeam);
   });
  });
 }
 
-// Build an array of Employees based on answers provided by the user
-function getTeam(mgrAnswers, empAnswers) {
- const team =
- [
-  new Manager(
-   mgrAnswers.name,
-   mgrAnswers.id,
-   mgrAnswers.email,
-   mgrAnswers.officeNumber
-  )
- ];
- empAnswers.employees.forEach(emp => {
-  if (emp.role === "Intern") {
-   team.push(
-    new Intern(
-     emp.name,
-     emp.id,
-     emp.email,
-     emp.school
-    )
-   );
-  } else {
-   team.push(
-    new Engineer(
-     emp.name,
-     emp.id,
-     emp.email,
-     emp.github
-    )
-   );
-  }
- });
-
- return team;
-}
-
-// Start the app by prompting for info
-getEmployees();
+// Start the app
+init();
